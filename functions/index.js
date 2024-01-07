eebec13990bfc7f6d146f360451b10b2313c91ed
Bhurtel Maheshwor <bhurtelace@gmail.com>
2024-01-07 10:12:35 +0900

    ages

--- functions/index.js
+++ functions/index.js
@@ -93,7 +93,7 @@ console.log("Your Life Path Number: " + lifePathNumber);
         }
     
         // Output the information based on the life path number
-        console.log("Life Path Number " + lifePathNumber + ":");
+        // console.log("Life Path Number " + lifePathNumber + ":");
     
         for (var i = 0; i < ageRanges.length; i++) {
             console.log("The Ages of Your " + getPinnacleOrdinal(i + 1) + " Pinnacle: " +
@@ -102,7 +102,7 @@ console.log("Your Life Path Number: " + lifePathNumber);
         }
     
         // Output the age range for the fourth pinnacle
-        console.log("The Ages of Your 4th Pinnacle: " + startingAge + "+");
+        // console.log("The Ages of Your 4th Pinnacle: " + startingAge + "+");
     }
     
     //Helper function to get the ordinal suffix for the pinnacle
@@ -121,11 +121,77 @@ console.log("Your Life Path Number: " + lifePathNumber);
     calculatePinnacleYear(SUM);
 
 
+    function getPinnacleAges(SUM) {
+        // Define the starting age for the first pinnacle
+        var startingAge = 0;
+    
+        // Define the age ranges for each pinnacle based on life path number
+        var ageRanges = [];
+    
+        switch (SUM) {
+            case 1:
+                ageRanges = [35, 44, 53];
+                break;
+            case 2:
+                ageRanges = [34, 43, 52];
+                break;
+            case 3:
+                ageRanges = [33, 42, 51];
+                break;
+            case 4:
+                ageRanges = [32, 41, 50];
+                break;
+            case 5:
+                ageRanges = [31, 40, 49];
+                break;
+            case 6:
+                ageRanges = [30, 39, 48];
+                break;
+            case 7:
+                ageRanges = [29, 38, 47];
+                break;
+            case 8:
+                ageRanges = [28, 37, 46];
+                break;
+            case 9:
+                ageRanges = [27, 36, 45];
+                break;
+            default:
+                console.error("Invalid life path number");
+                return;
+        }
+    
+        // Create an array of objects representing age periods for each pinnacle
+        var pinnacleAges = [];
+        for (var i = 0; i < ageRanges.length; i++) {
+            pinnacleAges.push({
+                pinnacle: i + 1,
+                agePeriod: startingAge + " - " + ageRanges[i]
+            });
+            startingAge = ageRanges[i] + 1;
+        }
+    
+        // Add the age range for the fourth pinnacle
+        pinnacleAges.push({
+            pinnacle: 4,
+            agePeriod: startingAge + "+"
+        });
+    
+        return pinnacleAges;
+    }
+
+    let pinnacleAgePeriod = pinnacleAges.forEach(function (pinnacle) {
+        return ("Your " + getPinnacleOrdinal(pinnacle.pinnacle) + " Pinnacle Age Period: " + pinnacle.agePeriod);
+    });
+
+
     //Calculate Pinnacle numbers:
     let firstPinnacleNumber = reduceToSingle(month + day);
+    
     let secondPinnacleNumber = reduceToSingle(day + year);
     let thirdPinnacleNumber = reduceToSingle(firstPinnacleNumber + secondPinnacleNumber);
     let fourthPinnacleNumber = reduceToSingle(month + year);
+    console.log(pinnacleAgePeriod);
     console.log("Your first pinnacle number is: " + firstPinnacleNumber);
     console.log("Your second pinnacle number is: " + secondPinnacleNumber);
     console.log("Your third pinnacle number is: " + thirdPinnacleNumber);
@@ -164,6 +230,9 @@ console.log("Your Life Path Number: " + lifePathNumber);
     <h4 class="lifePathNumber">Your Life Path Number is ${lifePathNumber} 
         <br>
         <p>hello</p>
+    </h4>
+    <h4 class="pinnacleAgePeriod"> Your Pinnacle age period: <br>
+        <p>${pinnacleAgePeriod}<p>
     </h4>
     <h4 class="firstPinnacleNumber">Your 1st Pinnacle Number is ${firstPinnacleNumber} <br> <p> </p>
     </h4>
