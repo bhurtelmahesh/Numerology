83f8ddd4c8d7790ecd6fe1361dd4050bc6958172
bhurtelmahesh <bhurtelace@gmail.com>
2024-01-07 20:12:16 +0900

    tried

---
+++ functions/lifePath.js
@@ -0,0 +1,25 @@
+switch (position) {
+    case "first":
+        switch (pinnacleNumber) {
+            case 1:
+                return "<p>Your early years are focused on building a strong foundation...</p>";
+            case 2:
+                return "<p>You’re afforded opportunities to take on challenges and leadership roles...</p>";
+            case 3:
+                return "<p>You’re afforded opportunities to take on challenges and leadership roles...</p>";
+            case 4:
+                return "<p>You have the opportunity to be courageous and use your determination to excel at whatever you undertake...</p>";
+            case 5:
+                return "<p>You have the opportunity to be courageous and use your determination to excel at whatever you undertake...</p>";
+            case 6:
+                return "<p>You have the opportunity to be courageous and use your determination to excel at whatever you undertake...</p>";
+            case 7:
+                return "<p>You have the opportunity to be courageous and use your determination to excel at whatever you undertake...</p>";
+            case 8:
+                return "<p>You have the opportunity to be courageous and use your determination to excel at whatever you undertake...</p>";
+            case 9:
+                return "<p>You have the opportunity to be courageous and use your determination to excel at whatever you undertake...</p>";
+            default:
+                return "<p>Unavailable</p>";
+        }
+}
